b2c82bcc4d098ca90f357d7a7488d7afe9663a5c
Maxel-Uds <maxellopes32@gmail.com>
2021-11-19 17:44:49 -0300

    Nivelamento JPA - parte 3

--- Nivelamento_JPA/aulajpamaven/src/main/java/aplicacao/Programa.java
+++ Nivelamento_JPA/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -2,16 +2,39 @@ package aplicacao;
 
 import dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Programa {
 
     public static  void main(String[] args) {
-        Pessoa p1 = new Pessoa(1, "Carlos da Silva", "blablabla@email.com");
-        Pessoa p2 = new Pessoa(2, "João da Silva", "blablabla@email.com");
-        Pessoa p3 = new Pessoa(3, "Ana Maria", "blablabla@email.com");
+//        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "blablabla@email.com");
+//        Pessoa p2 = new Pessoa(null, "João da Silva", "blablabla@email.com");
+//        Pessoa p3 = new Pessoa(null, "Ana Maria", "blablabla@email.com");
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+//          Valor encontrado no persistence.xml linha 7
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+//        Quando o JPA faz uma operção que não é só uma leitura de dados ele precisa de uma transação
+//        em.getTransaction().begin();
+//        Salvando no DB
+//        em.persist(p1);
+//        em.persist(p2);
+//        em.persist(p3);
+//        Corfirmando as alterações
+//        em.getTransaction().commit();
+
+//        Buscando pessoa
+        Pessoa p = em.find(Pessoa.class, 2);
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
+        System.out.println("pronto");
+
+        em.close();
+        emf.close();
     }
 
 }
--- Nivelamento_JPA/aulajpamaven/src/main/java/dominio/Pessoa.java
+++ Nivelamento_JPA/aulajpamaven/src/main/java/dominio/Pessoa.java
@@ -1,16 +1,24 @@
 package dominio;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String nome;
     private String email;
 
     public Pessoa() {
     }
 
-    public Pessoa(int id, String nome, String email) {
+    public Pessoa(Integer id, String nome, String email) {
         this.id = id;
         this.nome = nome;
         this.email = email;
