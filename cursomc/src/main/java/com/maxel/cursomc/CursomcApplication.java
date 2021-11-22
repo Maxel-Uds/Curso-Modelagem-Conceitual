67f21fe4a43eaaa2cc4f2cc9388c5171d304feda
Maxel-Uds <maxellopes32@gmail.com>
2021-11-22 19:46:33 -0300

    Criando a operação de instanciação

--- cursomc/src/main/java/com/maxel/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/maxel/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.maxel.cursomc;
 
+import com.maxel.cursomc.domain.Categoria;
+import com.maxel.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Escritório");
+		Categoria cat2 = new Categoria(null, "Informática");
+
+		repo.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
