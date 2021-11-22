bf87d0585d0cbac4874db95b110b507074d39557
Maxel-Uds <maxellopes32@gmail.com>
2021-11-22 16:06:45 -0300

    Testando a primeira classe de dominio - Categoria

--- cursomc/src/main/java/com/maxel/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/maxel/cursomc/resources/CategoriaResource.java
@@ -1,18 +1,26 @@
 package com.maxel.cursomc.resources;
 
+import com.maxel.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    private Integer id;
-    private String categoria;
-
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Funcionando";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> list = new ArrayList<Categoria>();
+        list.add(cat1);
+        list.add((cat2));
+
+        return  list;
     }
 }
