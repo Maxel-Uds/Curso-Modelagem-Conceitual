28ecd08f5fbda47aaceb9e721963b17414fdd3cd
Maxel-Uds <maxellopes32@gmail.com>
2021-11-27 17:15:06 -0300

    Validação customizada: email não repetido na inserção do Cliente

--- cursomc/src/main/java/com/maxel/cursomc/repositories/ClienteRepository.java
+++ cursomc/src/main/java/com/maxel/cursomc/repositories/ClienteRepository.java
@@ -2,6 +2,10 @@ package com.maxel.cursomc.repositories;
 
 import com.maxel.cursomc.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly = true) //Indica que não vai envolver uma operação de banco de dados
+    Cliente findByEmail(String email);
 }
