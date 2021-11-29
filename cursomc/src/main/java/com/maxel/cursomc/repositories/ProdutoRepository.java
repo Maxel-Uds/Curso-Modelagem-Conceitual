b22249a9397489786e044dd310d9df4c21251b60
Maxel-Uds <maxellopes32@gmail.com>
2021-11-29 15:56:59 -0300

    Busca de pedidos por nome e categorias - parte 1

--- cursomc/src/main/java/com/maxel/cursomc/repositories/ProdutoRepository.java
+++ cursomc/src/main/java/com/maxel/cursomc/repositories/ProdutoRepository.java
@@ -1,7 +1,17 @@
 package com.maxel.cursomc.repositories;
 
+import com.maxel.cursomc.domain.Categoria;
 import com.maxel.cursomc.domain.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+    @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias.cat WHERE obj.name LIKE %:nome% AND cat IN :categorias")
+    Page<Produto> search(@Param("nome") String nome, @Param("categorias")List<Categoria> categorias, PageRequest pageRequest);
 }
---
+++ cursomc/src/main/java/com/maxel/cursomc/resources/utils/URL.java
@@ -0,0 +1,13 @@
+package com.maxel.cursomc.resources.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class URL {
+
+    public static List<Integer> decodeIntList(String s) {
+        return Arrays.asList(s.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+    }
+}
