b75c8efb7992c3d1b6ade4349e73d6c2f9c20361
Maxel-Uds <maxellopes32@gmail.com>
2021-12-08 11:17:24 -0300

    Configuração inicial do Spring Security

---
+++ src/main/java/com/maxel/cursomc/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.maxel.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment environment;
+
+    //Lista dos caminhos que vão estar liberados
+    private static final String[] PUBLIC_MATCHERS = {
+          "/h2-console/**",
+    };
+
+    //Lista dos caminhos que vão estar liberados apenas para leitura
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/produtos/**",
+            "/categorias/**"
+    };
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        if(Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            httpSecurity.headers().frameOptions().disable();
+        }
+
+        //Autoriza todas as requisições aos caminhos definidos no PUBLIC_MATCHERS e somente leitura para os caminhos do PUBLIC_MATCHERS_GET
+        //E define que para o resto é preciso autenticação
+        httpSecurity.cors().and().csrf().disable();
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated();
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        //Permite o acesso por múltiplas fontes a todos os caminhos, mas apenas com as configurações básicas
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
